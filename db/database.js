6b613c2c52481b19bda621fad23e01dad79fd357
TheSpecialNone <friesareepic666@gmail.com>
2025-07-27 18:20:51 +0100

    Changed database

--- db/database.js
+++ db/database.js
@@ -1,24 +1,47 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./db/contracts.db');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-db.run(`CREATE TABLE IF NOT EXISTS contracts (
-  userId TEXT PRIMARY KEY,
-  teamName TEXT,
-  emoji TEXT
-)`);
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+const contractSchema = new mongoose.Schema({
+  userId: { type: String, required: true, unique: true },
+  teamName: String,
+  emoji: String,
+});
+
+const Contract = mongoose.model('Contract', contractSchema);
 
 module.exports = {
-  getContractedTeam: (userId, callback) => {
-    db.get('SELECT teamName, emoji FROM contracts WHERE userId = ?', [userId], (err, row) => {
-      callback(err, row);
-    });
+  getContractedTeam: async (userId, callback) => {
+    try {
+      const contract = await Contract.findOne({ userId });
+      callback(null, contract);
+    } catch (err) {
+      callback(err);
+    }
   },
 
-  contractPlayer: (userId, teamName, emoji, callback) => {
-    db.run('INSERT INTO contracts (userId, teamName, emoji) VALUES (?, ?, ?)', [userId, teamName, emoji], callback);
+  contractPlayer: async (userId, teamName, emoji, callback) => {
+    try {
+      await Contract.updateOne(
+        { userId },
+        { $set: { teamName, emoji } },
+        { upsert: true }
+      );
+      callback(null);
+    } catch (err) {
+      callback(err);
+    }
   },
 
-  releasePlayer: (userId, callback) => {
-    db.run('DELETE FROM contracts WHERE userId = ?', [userId], callback);
+  releasePlayer: async (userId, callback) => {
+    try {
+      await Contract.deleteOne({ userId });
+      callback(null);
+    } catch (err) {
+      callback(err);
+    }
   }
 };
