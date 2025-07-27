dc796c82b20e5806cf6427d279b48503567501c0
TheSpecialNone <friesareepic666@gmail.com>
2025-07-27 19:00:36 +0100

    logging uri

--- db/database.js
+++ db/database.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+console.log(process.env.MONGO_URI)
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
