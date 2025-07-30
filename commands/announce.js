80084454b32c199ef26710103e6de08ac6268cb3
TheSpecialNone <friesareepic666@gmail.com>
2025-07-30 16:37:26 +0100

    rechanges to announce, ne wperms

--- commands/announce.js
+++ commands/announce.js
@@ -1,6 +1,7 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, PermissionsBitField } = require('discord.js');
 
-const ANNOUNCE_CHANNEL_ID = '1400122725317607586'; 
+const ANNOUNCE_CHANNEL_ID = '1400122725317607586';
+const MINIMUM_ROLE_ID = '1335618455251980330';
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -17,8 +18,22 @@ module.exports = {
     const message = interaction.options.getString('message');
     const user = interaction.user;
 
+    const member = interaction.member;
+    const guild = interaction.guild;
+
+    const requiredRole = guild.roles.cache.get(MINIMUM_ROLE_ID);
+    if (!requiredRole) {
+      return interaction.reply({ content: '❌ Role restriction is misconfigured. Contact an admin.', ephemeral: true });
+    }
+
+    const hasPermission = member.roles.highest.comparePositionTo(requiredRole) >= 0;
+
+    if (!hasPermission) {
+      return interaction.reply({ content: '🚫 You do not have permission to use this command.', ephemeral: true });
+    }
+
     const embed = new EmbedBuilder()
-      .setColor('#f2f2f2') // test
+      .setColor('#f2f2f2')
       .setDescription(message)
       .setTimestamp()
       .setFooter({
