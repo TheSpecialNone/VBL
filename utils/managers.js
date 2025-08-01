6740538fe00bb106f7e82e1429804d0a084df4c2
TheSpecialNone <friesareepic666@gmail.com>
2025-08-01 18:18:02 +0100

    added new managers

--- utils/managers.js
+++ utils/managers.js
@@ -1,13 +1,77 @@
 module.exports = {
-  enabled: false, 
+  enabled: false,
   managers: {
-
-    /*
-    '852970957861027860': {
+    '650271397762957322': {
       team: 'Arsenal',
-      emoji: '<:ARS:1398677897035776152>'
+      emoji: '<:ARS:1400202180081877073>'
+    },
+    '1247069568497877065': {
+      team: 'Chelsea',
+      emoji: '<:CHE:1400202177682604294>'
+    },
+    '953317964583686234': {
+      team: 'Liverpool',
+      emoji: '<:LIV:1400202174935470101>'
+    },
+    '526467593049997354': {
+      team: 'Liverpool',
+      emoji: '<:LIV:1400202174935470101>'
+    },
+    '1377337166824472606': {
+      team: 'Man City',
+      emoji: '<:MCI:1400202173203349514>'
+    },
+    '873416642250547230': {
+      team: 'Man City',
+      emoji: '<:MCI:1400202173203349514>'
+    },
+    '892382982181126194': {
+      team: 'Man United',
+      emoji: '<:MUN:1400202169595986031>'
+    },
+    '859476106075963413': {
+      team: 'Tottenham',
+      emoji: '<:TOT:1400202166920151180>'
+    },
+    '711223937635319809': {
+      team: 'Tottenham',
+      emoji: '<:TOT:1400202166920151180>'
+    },
+    '1284449163756437544': {
+      team: 'Athletic Bilbao',
+      emoji: '<:ATH:1400202577274212402>'
+    },
+    '225661506040102913': {
+      team: 'Atletico Madrid',
+      emoji: '<:ATM:1400202574971408544>'
+    },
+    '537783462481100821': {
+      team: 'Atletico Madrid',
+      emoji: '<:ATM:1400202574971408544>'
+    },
+    '650825435696988180': {
+      team: 'FC Barcelona',
+      emoji: '<:FCB:1400202571670487254>'
+    },
+    '899718011521490975': {
+      team: 'FC Barcelona',
+      emoji: '<:FCB:1400202571670487254>'
+    },
+    '1254189304742150219': {
+      team: 'Real Madrid',
+      emoji: '<:RMA:1400202568742998067>'
     },
 
+   
+    /*
+    'REAL_SOCIEDAD_MANAGER_ID': {
+      team: 'Real Sociedad',
+      emoji: '<:RSO:1400889695533273209>'
+    },
+    'SEVILLA_MANAGER_ID': {
+      team: 'Sevilla',
+      emoji: '<:SEV:1400889809169289339>'
+    },
     */
   }
 };
